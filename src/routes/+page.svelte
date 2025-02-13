0d7d4651eda80e82b0627901f909c1b1b2bd1155
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-02-14 01:56:40 +0530

    formatting and stuff :/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
-
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button/index.js";
-   </script>
-    
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-
-   <Button>Click me</Button>
+<Button>Click me</Button>
