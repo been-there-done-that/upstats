0ee11040e94c1fcbacc7ccfd5dd3da1d507eea48
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-02-14 20:27:21 +0530

    format: formatting and stuff :/

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import { Toaster } from "$lib/components/ui/sonner/index.js";
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-
-<Toaster position='top-right'/>
+<Toaster position="top-right" />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-  import { toast } from "svelte-sonner";
-
+	import { toast } from 'svelte-sonner';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -9,17 +8,16 @@
 
 <Button>Click me</Button>
 
-
 <Button
-  variant="outline"
-  onclick={() =>
-    toast.success("Event has been created", {
-      description: "Sunday, December 03, 2023 at 9:00 AM",
-      action: {
-        label: "Undo",
-        onClick: () => console.info("Undo")
-      }
-    })}
+	variant="outline"
+	onclick={() =>
+		toast.success('Event has been created', {
+			description: 'Sunday, December 03, 2023 at 9:00 AM',
+			action: {
+				label: 'Undo',
+				onClick: () => console.info('Undo')
+			}
+		})}
 >
-  Show Toast
+	Show Toast
 </Button>
