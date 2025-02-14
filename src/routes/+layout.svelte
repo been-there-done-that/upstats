aa5355b6a45daad2d33edb9eb47ded79a514cae2
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-02-14 20:26:42 +0530

    chore: added the svelte sonnar now :/

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import { Toaster } from "$lib/components/ui/sonner/index.js";
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+
+<Toaster position='top-right'/>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
+	import { Button } from '$lib/components/ui/button';
+  import { toast } from "svelte-sonner";
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <Button>Click me</Button>
+
+
+<Button
+  variant="outline"
+  onclick={() =>
+    toast.success("Event has been created", {
+      description: "Sunday, December 03, 2023 at 9:00 AM",
+      action: {
+        label: "Undo",
+        onClick: () => console.info("Undo")
+      }
+    })}
+>
+  Show Toast
+</Button>
