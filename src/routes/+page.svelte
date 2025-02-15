a189d7e118241351f6cf65f4c485c0b261a2a788
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-02-15 16:50:19 +0530

    chore: added the Beats to shown the page :/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,10 @@
 	import Ellipsis from 'lucide-svelte/icons/ellipsis';
 	import * as Table from '$lib/components/ui/table/index.js';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+	import { cn } from '$lib/utils';
 
-	// let open = $state(false)
+	const BEATS_ITEMS = 10;
 
 	let selectedRowForDelete: null | any = $state(null);
 	let open = $state(false);
@@ -19,45 +21,98 @@
 		{
 			name: 'http://google.com',
 			url: 'http://google.com',
-			totalAmount: '$250.00',
+			beats: [
+				{
+					date: '2025-02-15T10:27:01Z',
+					status_code: 400
+				},
+				{
+					date: '2025-02-15T10:28:01Z',
+					status_code: 200
+				},
+				{
+					date: '2025-02-15T10:29:01Z',
+					status_code: 200
+				}
+			],
 			frequency: '5 Min'
 		}
 	];
+
+	const BEATS_COLOR_MAPPING: { [key: number]: string } = {
+		200: 'bg-green-400',
+		201: 'bg-green-400',
+		300: 'bg-green-400',
+		400: 'bg-red-600',
+		401: 'bg-red-600',
+		402: 'bg-red-600',
+		403: 'bg-red-600',
+		404: 'bg-red-600',
+		405: 'bg-red-600',
+		500: 'bg-purple-500'
+	};
 </script>
 
 <h1 class="text-2xl font-extrabold italic">Monitors</h1>
 
-{open}
-{JSON.stringify(selectedRowForDelete)}
+<div class="m-20">
+	<div class="flex flex-row-reverse gap-1.5">
+		{#each { length: BEATS_ITEMS } as _, idx}
+			{@const value = invoices[0].beats[idx]}
+			<Tooltip.Provider>
+				<Tooltip.Root>
+					<Tooltip.Trigger
+						><div
+							class={cn(
+								'h-8 w-1.5 rounded-md',
+								BEATS_COLOR_MAPPING[value?.status_code] ?? 'bg-black/10 dark:bg-white/20'
+							)}
+						></div></Tooltip.Trigger
+					>
+					<Tooltip.Content
+						class={BEATS_COLOR_MAPPING[value?.status_code] ?? 'bg-black dark:bg-white'}
+					>
+						<div
+							class={cn(
+								!value ? 'text-white dark:text-black' : 'text-black',
+								'font-semibold italic'
+							)}
+						>
+							{value?.date ?? 'Not Ran Yet'}{value ? ` - ${value.status_code}` : ''}
+						</div>
+					</Tooltip.Content>
+				</Tooltip.Root>
+			</Tooltip.Provider>
+		{/each}
+	</div>
+</div>
+
 <div class="my-8 w-full rounded-lg border text-gray-500 shadow-md hover:text-black dark:text-white">
 	<Table.Root>
 		<Table.Header>
 			<Table.Row>
-				<Table.Head class="pl-5 text-left italic text-black">Name</Table.Head>
-				<Table.Head class="text-center italic text-black">URL</Table.Head>
-				<Table.Head class="text-center italic text-black">Beats</Table.Head>
-				<Table.Head class="w-24 text-center italic text-black">Frequency</Table.Head>
-				<Table.Head class="w-24 text-center italic text-black">Action</Table.Head>
+				<Table.Head class="pl-5 text-left italic text-black dark:text-white">Name</Table.Head>
+				<Table.Head class="text-center italic text-black dark:text-white">URL</Table.Head>
+				<Table.Head class="text-center italic text-black dark:text-white">Beats</Table.Head>
+				<Table.Head class="w-24 text-center italic text-black dark:text-white">Frequency</Table.Head
+				>
+				<Table.Head class="w-24 text-center italic text-black dark:text-white">Action</Table.Head>
 			</Table.Row>
 		</Table.Header>
 		<Table.Body>
 			{#each invoices as invoice, i (i)}
 				<Table.Row class="group">
-					<Table.Cell
-						class="border pl-5 text-left text-gray-500 group-hover:text-black dark:text-white"
+					<Table.Cell class="pl-5 text-left text-gray-500 group-hover:text-black dark:text-white"
 						>{invoice.name}</Table.Cell
 					>
-					<Table.Cell
-						class="-p-2 border text-center text-gray-500 group-hover:text-black dark:text-white"
+					<Table.Cell class="-p-2 text-center text-gray-500 group-hover:text-black dark:text-white"
 						>{invoice.url}</Table.Cell
 					>
-					<Table.Cell
-						class="-p-2 border text-center text-gray-500 group-hover:text-black dark:text-white"
-						>{invoice.frequency}</Table.Cell
+					<Table.Cell class="-p-2 text-center text-gray-500 group-hover:text-black dark:text-white"
+						>{invoice.beats}</Table.Cell
 					>
-					<Table.Cell
-						class="-p-2 border text-center text-gray-500 group-hover:text-black dark:text-white"
-						>{invoice.totalAmount}</Table.Cell
+					<Table.Cell class="-p-2 text-center text-gray-500 group-hover:text-black dark:text-white"
+						>{invoice.frequency}</Table.Cell
 					>
 					<Table.Cell class="-p-2 text-center text-black dark:text-white">
 						<DropdownMenu.Root>
